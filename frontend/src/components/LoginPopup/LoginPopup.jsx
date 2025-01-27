e2dfb65f92bdc003518cf64193d52cc22a9ea132
SaGerz <genarosamuel24@gmail.com>
2025-01-28 01:30:49 +0700

    Feat: Add login standard authenticate and add token

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios';
 
 const LoginPopup = ({setShowLogin}) => {
   const [currState, setCureState] = useState("Login")
@@ -10,34 +12,62 @@ const LoginPopup = ({setShowLogin}) => {
     password: ""
   })
 
+  const {url, setToken} = useContext(StoreContext);
+
   const onChangeHandlder = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     setData(data => ({...data, [name]: value}));
   }
 
-  useEffect(() => {
-    console.log(data);
-  }, [data])
+  const onLogin = async (event) => {
+    event.preventDefault();
+    let newUrl = url;
+    if(currState === "Login") 
+    {
+      newUrl += '/api/user/login';
+    } else {
+      newUrl +=  '/api/user/register';
+    }
+
+    try {
+      const response = await axios.post(newUrl, data);
+      console.log(response);
+
+      if(response.data.success)
+      {
+        setToken(response.data.token);
+        localStorage.setItem('Token', response.data.token);
+        setShowLogin(false);
+      } else {
+        alert(response.data.message)
+      }
+
+      // console.log('Succes');
+    } catch (error) {
+      console.log(`Error : ${error}`);
+    }
+
+  }
 
   console.log(currState)
   return (
     <div className="login-popup">
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
           <div className="login-popup-title">
             <h2>{currState}</h2>
             <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
           </div>
           <div className="login-popup-inputs">
           {currState==="Login" ?  
-            <></> : <input name='name' onChange={onChangeHandlder} value={data.name} type="text" placeholder='Your name' /> 
+            <></> : <input name='name' onChange={onChangeHandlder} value={data.name} type="text" placeholder='Your name' required/> 
           }
-            <input name='email' onChange={onChangeHandlder} value={data.email} type="email" placeholder='Your email' />
-            <input name='password' onChange={onChangeHandlder} value={data.password} type="password" placeholder='Your password' />
+            <input name='email' onChange={onChangeHandlder} value={data.email} type="email" placeholder='Your email' required/>
+            <input name='password' onChange={onChangeHandlder} value={data.password} type="password" placeholder='Your password' required/>
           </div>
-          <button>{currState==="Sign Up" ? "Create Account" : "Login"}</button>
+          <button type='submit' >{currState==="Sign Up" ? "Create Account" : "Login"}</button>
           <div className="login-popup-condition">
-            <input type="checkbox" />
+            <input type="checkbox" required/>
             <p>By continuing, i agree to the terms of use privacy & policy.</p>
           </div>
           {currState==="Login" ? 
