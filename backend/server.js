a58a0aa734de9a95ccca28d4ab58b2ff8dae4cc2
SaGerz <genarosamuel24@gmail.com>
2025-04-01 16:35:26 +0700

    feat: Add route controller cart API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDb } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import "dotenv/config"
+import cartRouter from './routes/cartRoute.js';
 
 // App config
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware untuk parsing URL
 app.use('/api/food', foodRouter)
 app.use('/images', express.static('uploads'))
 app.use('/api/user', userRouter)
+app.use('/api/cart', cartRouter)
 
 // DB Connection 
 connectDb();
